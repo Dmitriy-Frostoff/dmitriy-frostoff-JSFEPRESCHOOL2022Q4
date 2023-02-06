df97d1ae1db41ecb791aee5f650c175c426d00e0
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-02-07 00:45:34 +0300

    refactor: refactor code of logic of activation price's accordion buttons

--- plants/js/plantsPart3/prices__accordion-button__js.js
+++ plants/js/plantsPart3/prices__accordion-button__js.js
@@ -8,6 +8,7 @@ window.onload = function () {
 
 const pricesOptionsButtonHandler = () => {
   const prices__optionsContainer = document.querySelector('.prices__options-container');
+
   prices__optionsContainer.addEventListener('click', (event) => {
     const prices__AllButtonsOfAccordion = document.querySelectorAll('.prices__accordion-button');
 
@@ -15,40 +16,24 @@ const pricesOptionsButtonHandler = () => {
 
     if ( clickedElem.closest('.prices__accordion-button') ) {
 
-      // //test area start
-      // // console.log( !(clickedElem.dataset.buttonId === [...prices__AllButtonsOfAccordion].find(getActiveButton)?.dataset.buttonId) );
-      // console.log(clickedElem.classList === 'prices__accordion-button_active');
-
-      // //experiment
-      // if ( !clickedElem.classList.contains( 'prices__accordion-button_active' ) &&
-      //   !prices__optionsContainer.outerHTML.includes( 'prices__accordion-button_active' )
-      // ) {
-      //   console.log(`1 do addSelectionToButton`);
-      //   addSelectionToButton(clickedElem);
-      // }
-      
-      
-      // return;
-      // //test area end
-
-      if ( !clickedElem.classList.contains( 'prices__accordion-button_active' ) &&
-        !prices__optionsContainer.outerHTML.includes( 'prices__accordion-button_active' )
+      if ( !clickedElem.classList.contains('prices__accordion-button_active') &&
+        !prices__optionsContainer.outerHTML.includes('prices__accordion-button_active')
       ) {
-        console.log(`1 do addSelectionToButton`);
+        // console.log(`1 do addSelectionToButton`);
         addSelectionToButton(clickedElem);
       }
 
       else if ((clickedElem.dataset.buttonId === [...prices__AllButtonsOfAccordion].find(compareIdsClickedElemAllAccordionButtons)?.dataset.buttonId) &&
         clickedElem.classList.contains('prices__accordion-button_active')
         ) {
-          console.log(`2 do removeSelectionFromButton`);
+          // console.log(`2 do removeSelectionFromButton`);
           removeSelectionFromButton();
       }
 
       
       else if ( !(clickedElem.dataset.buttonId === [...prices__AllButtonsOfAccordion].find(getActiveButton)?.dataset.buttonId) && 
-        prices__optionsContainer.outerHTML.includes( 'prices__accordion-button_active' ) ) {
-          console.log(`3 do removeSelection addSelection`);
+        prices__optionsContainer.outerHTML.includes( 'prices__accordion-button_active')) {
+          // console.log(`3 do removeSelection addSelection`);
           removeSelectionFromButton();
           addSelectionToButton(clickedElem);
       }
@@ -62,14 +47,13 @@ const pricesOptionsButtonHandler = () => {
 
 const compareIdsClickedElemAllAccordionButtons = (elem) => {
   let clickedElem = event.target;
-  if ( elem.dataset.buttonId === clickedElem.dataset.buttonId ) {
+  if (elem.dataset.buttonId === clickedElem.dataset.buttonId) {
     return elem.dataset.buttonId;
   }
 }
 
 const getActiveButton = (item) => {
-  if ( item.classList === 'prices__accordion-button_active' ) {
-    console.log(`item.classList`);
+  if (item.getAttribute('class').includes('prices__accordion-button_active')) {
     return item;
   }
 }
@@ -126,9 +110,10 @@ const addSelectionToButton = (clickedElem) => {
 }
 
 //I created this code for 4 ours!!! But it hard to read, so it need to be refactored((((((((( ::crying::
+
 const linkDescriptionWithButton = (clickedElem, buttonDescriptionSync) => {
   Object.keys(buttonDescriptionSync).forEach( (elem) => {
-    if ( elem === clickedElem.dataset.buttonId ) {
+    if (elem === clickedElem.dataset.buttonId) {
       let elemWatch = document.querySelectorAll('.prices__accordion-description-container');
       elemWatch.forEach((item) => {
         if (item.dataset.button__descriptionId === buttonDescriptionSync[elem]) {
@@ -141,7 +126,7 @@ const linkDescriptionWithButton = (clickedElem, buttonDescriptionSync) => {
 
 const linkArrowsWithButton = (clickedElem, buttonArrowsSync) => {
   Object.keys(buttonArrowsSync).forEach( (elem) => {
-    if ( elem === clickedElem.dataset.buttonId ) {
+    if (elem === clickedElem.dataset.buttonId) {
       let elemWatch = document.querySelectorAll('.prices__accordion-button_vector');
       elemWatch.forEach((item) => {
         if (item.dataset.button__vectorId === buttonArrowsSync[elem]) {
@@ -151,69 +136,3 @@ const linkArrowsWithButton = (clickedElem, buttonArrowsSync) => {
     }
   })
 }
-// TODO   it doesn't work delete after few days if useless
-// const linkDescriptionWithButton = (clickedElem, buttonDescriptionSync) => {
-//   let clickedElementID = clickedElem.dataset.buttonId;
-//   if ( Object.keys(buttonDescriptionSync).includes(clickedElementID) ) {
-//     let elemWatch = document.querySelectorAll('.prices__accordion-description-container');
-//     console.log(`elemWatch ${elemWatch}`);
-//     if ( elemWatch.includes(buttonDescriptionSync[clickedElementID]) ) {
-//       elemWatch.find((item) => item === clickedElementID).classList.remove('prices__elem_hidden');
-//     }
-//   }
-
-// }
-
-// const removeSelectionToButton = (clickedElem) => {
-//   const prices__optionsContainer = document.querySelector('.prices__options-container');
-  
-//   const prices__accordionButton = document.querySelector('.prices__accordion-button');
-//   const prices__accordionButton_active = document.querySelector('.prices__accordion-button_active');
-//   const prices__accordionButton_vector = document.querySelector('.prices__accordion-button_vector');
-//   const prices__accordionDescriptionContainer = document.querySelector('.prices__accordion-description-container');
-
-//   const buttonDescriptionSync = {
-//     'prices__accordion-button1' : 'button-description1',
-//     'prices__accordion-button2' : 'button-description2',
-//     'prices__accordion-button3' : 'button-description3'
-//   };
-
-//   const buttonArrowsSync = {
-//     'prices__accordion-button1' : 'button-vector1',
-//     'prices__accordion-button2' : 'button-vector2',
-//     'prices__accordion-button3' : 'button-vector3'
-//   };
-  
-//   clickedElem.classList.toggle('prices__accordion-button_active');
-
-//   deleteLinkDescriptionWithButton(clickedElem, buttonDescriptionSync);
-
-//   deleteLinkArrowsWithButton(clickedElem, buttonArrowsSync);
-// }
-
-// //I created this code for 4 ours!!! But it hard to read, so it need to be refactored((((((((( ::crying::
-// const deleteLinkDescriptionWithButton = (clickedElem, buttonDescriptionSync) => {
-//   Object.keys(buttonDescriptionSync).forEach( (elem) => {
-//     if ( elem === clickedElem.dataset.buttonId ) {
-//       let elemWatch = document.querySelectorAll('.prices__accordion-description-container');
-//       elemWatch.forEach((item) => {
-//         if (item.dataset.button__descriptionId === buttonDescriptionSync[elem]) {
-//           return item.classList.toggle('prices__elem_hidden');
-//         }
-//       })
-//     }
-//   })
-// }
-
-// const deleteLinkArrowsWithButton = (clickedElem, buttonArrowsSync) => {
-//   Object.keys(buttonArrowsSync).forEach( (elem) => {
-//     if ( elem === clickedElem.dataset.buttonId ) {
-//       let elemWatch = document.querySelectorAll('.prices__accordion-button_vector');
-//       elemWatch.forEach((item) => {
-//         if (item.dataset.button__vectorId === buttonArrowsSync[elem]) {
-//           return item.classList.toggle('prices__accordion-button_vector-active');
-//         }
-//       })
-//     }
-//   })
-// }
